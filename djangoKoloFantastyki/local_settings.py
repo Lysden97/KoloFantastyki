a2c554e7b8cdc4759a6ceeb8e722c9f54a886f7c
Lysden97 <lysenko.denys97@gmail.com>
2025-01-14 19:32:10 +0100

    initial work

---
+++ djangoKoloFantastyki/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'KoloFantastyki',
+        'USER': 'postgres',
+        'PASSWORD': 'kolo85327532',
+        'HOST': 'localhost',
+        'PORT': '5433'
+    }
+}
